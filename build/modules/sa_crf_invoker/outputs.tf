30b437cddf5eaf323a0a9fb7715f798d03a42346
ilhamhanif <ilhamhanif12@gmail.com>
2024-08-31 21:04:41 +0700

    delete: terraform module sa_crf_invoker

--- build/modules/sa_crf_invoker/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "service_account_id" {
-  description = "SA ID"
-  value       = google_service_account.service_account.id
-}
-
-output "service_account_email" {
-  description = "SA Email"
-  value       = google_service_account.service_account.email
-}
--- build/modules/sa_crf_invoker/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "service_account_id" {
-  description = "SA ID"
-  type        = string
-}
-
-variable "service_account_display_name" {
-  description = "SA Display Name"
-  type        = string
-}
