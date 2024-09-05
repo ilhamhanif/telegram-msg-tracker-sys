8737c611d0f98f180f181fa083053f5b25305980
ilhamhanif <ilhamhanif12@gmail.com>
2024-09-05 10:45:22 +0700

    update: change modules name from gcp_cf2_zip_to_gcs to gcp_cf_zip_to_gcs

--- build/resources/cf_tlgrm_msg_upd_forwarder/main.tf
+++ build/resources/cf_tlgrm_msg_upd_forwarder/main.tf
@@ -21,7 +21,7 @@ locals {
 
 # Generates a ZIP compressed file archieve of the source code.
 module "zip_cf2_gcs" {
-  source = "../../modules/gcp_cf2_zip_to_gcs"
+  source = "../../modules/gcp_cf_zip_to_gcs"
 
   cloud_functions2_name = local.cf_name
   gcs_zip_project_id    = var.project_id
